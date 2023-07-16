1e1c0bc532096836d6640e76ceaa3e53df020fc6
Kayla-Day <kayladayguimont@gmail.com>
2023-07-16 17:17:33 -0500

    updates

---
+++ src/FreeComponent.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function FreeComponent() {
+  const [message, setMessage] = useState("");
+
+  // useEffect automatically executes once the page is fully loaded
+  useEffect(() => {
+    // set configurations for the API call here
+    const configuration = {
+      method: "get",
+      url: "https://nodejs-mongodb-auth-app.herokuapp.com/free-endpoint",
+    };
+
+    // make the API call
+    axios(configuration)
+      .then((result) => {
+        // assign the message in our result to the message we initialized above
+        setMessage(result.data.message);
+      })
+      .catch((error) => {
+        error = new Error();
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-center">Free Component</h1>
+      <h3 className="text-center text-danger">{message}</h3>
+    </div>
+  );
+}
